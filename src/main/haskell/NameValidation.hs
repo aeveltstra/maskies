b78c78e22e5fcbdbe268f8a7a1d1644b4cc109bc
A.E.Veltstra <aev@sdf.org>
2021-02-11 12:40:33 -0500

    Better type safety and improved code readability in the name validator.

--- src/main/haskell/NameValidation.hs
+++ src/main/haskell/NameValidation.hs
@@ -8,15 +8,16 @@
     needed
     @author A.E.Veltstra
     @copyright A.E.Veltstra & T.R.Veltstra
-    @version 2.21.0210.600
+    @version 2.21.211.1239
 -}
 module NameValidation where
 import qualified Data.Text.Lazy as TL
---import qualified Data.Text.Lazy.IO as TLIO
 import qualified TextWrapper as TW
 
 import Prelude
 import qualified System.Random as R
+
+type UserName = TL.Text
 
 {- | These are the results recognized by the validation of the player’s name. -}
 data Result = AllGood
@@ -29,7 +30,7 @@ data Result = AllGood
                 deriving (Bounded, Read, Show, Eq, Enum)
 
 {- | Ensures that the player name is neither too long nor too short. And a special case is made for pi, because a certain someone found it necessary to boast their knowledge of the first 100 digits of pi. You know who you are. -}
-validate :: TL.Text -> Result
+validate :: UserName -> Result
 validate "" = TooShort
 validate player
   | 0 < (TL.count "3.1415" player) = Pie
@@ -39,17 +40,17 @@ validate player
   | isJeremy player = IsJeremy
   | otherwise = AllGood
 
-subsForTooLong :: [TL.Text]
+subsForTooLong :: [UserName]
 subsForTooLong
   = ["Aadhira", "Aadesh", "Saanvi", "Aaditi", "Aadit", "Anika", "Aisha",
      "Ananya", "Arav", "Alisha", "Kaavya", "Saatvik", "Ahan", "Abhi"]
 
-subsForTooShort :: [TL.Text]
+subsForTooShort :: [UserName]
 subsForTooShort
   = ["Elikapeka", "Kakalina", "Leimomi", "Mahaelani", "Waiola", 
      "Ekewaka", "Uluwehi", "Healani", "Lanakila", "Leialoha"]
 
-fnaf :: [TL.Text]
+fnaf :: [UserName]
 fnaf
   --"freddy", "gabriel", "jeremy", "fritz", "charlie", "charlotte", "clara"
   = ["fazbear", "chica", "bonnie", "foxy", "golden", "nightmare",
@@ -59,18 +60,18 @@ fnaf
      "phantom", "deedee", "funtimes", "fun times", "tape girl", 
      "vanny", "purple guy", "orange man"]
 
-isFnaf :: TL.Text -> Bool
+isFnaf :: UserName -> Bool
 isFnaf player = any (\ x -> 0 < (TL.count x y)) fnaf
   where y = TL.toLower player
 
-isBetty :: TL.Text -> Bool
+isBetty :: UserName -> Bool
 isBetty player = "betty" == (TL.toLower player)
 
-isJeremy :: TL.Text -> Bool
+isJeremy :: UserName -> Bool
 isJeremy player = "jeremy" == (TL.toLower player)
 
 {- | Given a specific name validation result, this function returns the passed-in name or something else. -}
-replace :: TL.Text -> Result -> R.StdGen -> TL.Text
+replace :: UserName -> Result -> R.StdGen -> UserName
 replace _ TooLong seed = pick seed subsForTooLong
 replace _ TooShort seed = pick seed subsForTooShort
 replace _ Pie _ = "Pie"
@@ -84,7 +85,7 @@ pick seed xs
   where x = xs !! fst (R.randomR (0, (length xs) - 1) seed)
 
 {- | If the validation of the player’s name says it should be changed, this function lets them know about it. -}
-show :: Result -> TL.Text -> IO ()
+show :: Result -> UserName -> IO ()
 show AllGood _ = return ()
 show TooLong newName = TW.wrap (TL.replace "{name}" newName
   "That’s a really long name, you know. I will call you {name}.")
